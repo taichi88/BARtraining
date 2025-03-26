8de6f7061033fa093f7330d41cb1005e62ffc835
taichi88 <nikoloz.gabeskiria@gmail.com>
2025-03-26 15:26:14 +0400

    2.1 დავამატე კონტრაქტები (Iservices) და იმპლემენტაციები. 2.2 დავამატე ასინქრონული კონტროლერი, რომლითაც ვპოსტავ პესონის შექმნას. 2.3 დავამატე პერსონი მოდელი

---
+++ BankATMSystem.Api/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace BankATMSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpPost("CreatePerson")]
+        public async Task CreatePersonasync(PhysicalPersonModel PhysicalPersonModel)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Domain/Entities/PhysicalPersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Phisycal person model 
+namespace Domain.Entities
+{
+    public class PhysicalPersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IdentificationNumber { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
